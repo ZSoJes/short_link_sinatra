074b1bceb96286ce7e4db511e5dcd446f7f50f57
ZSoJes <GomezSoteloJuan@Gmail.com>
2016-10-26 12:43:33 -0500

    correccion de algunos errores

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,20 +1,50 @@
 get '/' do
    # Deja a los usuarios crear una URL reducida y despliega una lista de URLs. 
-   @short_url = params[:long_url]
+   @urls = Url.all
+   # @dato = Url.find_by(long_url: params[:long_url])
+
    erb :index
 end
 
 post '/urls' do
   # crea una nueva Url
-  # @short_url = params[:long_url]
+  @short_url = params[:long_url]
+  
+  puts @short_url
+  # puts :long_url
+  instancia = Url.new
+  @short_url = instancia.create_url_short
 
-  @ya_cortado = Url.new(@short_url)
-  erb :index
+  @url = Url.create(long_url: params[:long_url], short_url: @short_url)
+
+  if @url.save
+    # instancia.update(params[:long_url])
+
+    @short_url
+    redirect to('/')
+    # erb :short_url
+    erb :index
+  else 
+    redirect to('/error')
+  end
+
+end
+
+get '/error' do
+  erb :error
 end
 
 # e.g., /q6bda
 get '/:short_url' do
   # redirige a la URL original
-  @ya_cortado
-  erb :index
+  puts "::::::::::::::::::::::: "
+  @url_click = params[:short_url]  
+    x = Url.find_by({short_url: @url_click})
+    x.click_count += 1
+    x.save
+    # cont.click_count = cont.id
+
+    # cont.save
+ redirect to ("/#{x.long_url}")
+  # erb :index
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,45 +1,25 @@
 class Url < ActiveRecord::Base
 # @@contador = 0
 validates :long_url, presence: true, uniqueness: true
-# validates :click_count, presence: true
-# def initialize(long_url)
-#     @long_url = long_url
-# end
-# n = 1
-    
+validates :long_url, :format => { :with => URI::regexp(%w(http https)), :message => "Valid URL required"}    
     before_create :create_url_short
-  def create_url_short(long_url)
+
+  def create_url_short
 
     if self.long_url != nil
-        # self.short_url = long_url[0..4]
-        # self.increment_counter(:click_count, :id)
-
         uri = URI.parse(self.long_url)
-
-         if uri.scheme == "http" || uri.scheme == "https"
-        
             var = []
+
             6.times{ rand(0..1) == 1 ? var << rand(97..122).chr : var << rand(0..9) }
-
-             self.short_url = "#{uri.scheme}.cow.lo/#{var.join}"
-        else
-
-              self.short_url="no valido"
-        end
-
-
-        #self.click_count + 1
-        # self.click_count += 2
-        # self.click_count += 1
-# @@contador += 1
-        # self.click_count = @@contador
-        
-        # self.increment(:click_count)
-        # click_count.save
-        # click_count.reload
-       # self.click_count.save
+            self.short_url = "#{uri.scheme}://cow.lo/#{var.join}"
+            
     end # if
-
+  
   end#metodo
 
+  # def update(dato)
+  #   cont = Url.find_by(long_url: dato)
+  #   cont.click_count = cont.id
+  #   cont.save
+  # end
 end
